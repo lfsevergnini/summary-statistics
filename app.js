5a5c78e5c16fa70449494ba4f90110a89835fcaa
Luís Filipe Severgnini <luis.severgnini@pfizer.com>
2022-07-05 20:22:44 -0300

    Add employee 'repository'

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const employeeRepository = require('./repositories/employees');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const employees = employeeRepository.fetchAll();
+  res.send(employees);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
---
+++ repositories/employees.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const employees = JSON.parse(fs.readFileSync('repositories/employees.json'));
+
+const EmployeeRepository = {
+    fetchAll: () => employees,
+    insert: (employee) => employees.push(employee),
+    delete: (id) => {
+        employees = employees.filter(e => e.id !== id);
+    }
+}
+
+module.exports = EmployeeRepository;
