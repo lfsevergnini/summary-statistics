af599b6d617a1d033d094b09f396e1c1b4060b3f
Luís Filipe Severgnini <luis.severgnini@pfizer.com>
2022-07-06 09:21:05 -0300

    Add comparison method to calculate mean

--- repositories/employees.js
+++ repositories/employees.js
@@ -4,10 +4,24 @@ const employees = JSON.parse(fs.readFileSync('repositories/employees.json'));
 
 const EmployeeRepository = {
     fetchAll: () => employees,
+
     insert: (employee) => employees.push(employee),
+
     delete: (id) => {
         employees = employees.filter(e => e.id !== id);
-    }
+    },
+
+    compare: (employeeA, employeeB) => {
+        const [salaryA, salaryB] = [Number.parseInt(employeeA.salary), Number.parseInt(employeeB.salary)];
+
+        if (salaryA > salaryB)
+            return 1;
+
+        if (salaryA < salaryB)
+            return -1;
+
+        return 0;
+    },
 }
 
 module.exports = EmployeeRepository;
