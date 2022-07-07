9d7cffa22f08474667e7b2c38192831c56c46e59
Luís Filipe Severgnini <luis.severgnini@pfizer.com>
2022-07-07 10:01:02 -0300

    Add sub-departments SS endpoint

--- routes/statistics.js
+++ routes/statistics.js
@@ -5,6 +5,15 @@ const router = express.Router();
 const employeeRepository = require('../repositories/employees');
 const summaryStatistics = require('../statistics/summary');
 
+const getGroupStatistics = (department) => {
+  const salaries = department[1].map((emp) => emp.salary);
+
+  return {
+    department: department[0],
+    summary: summaryStatistics.getSummaryStatistics(salaries),
+  };
+};
+
 router.get('/', (req, res) => {
   const filter = req.query.on_contract ? (e) => e.on_contract : null;
 
@@ -30,14 +39,27 @@ router.get('/departments', (req, res) => {
     departments[e.department].push(e);
   });
 
-  const summary = Object.entries(departments).map((department) => {
-    const salaries = department[1].map((emp) => emp.salary);
+  const summary = Object.entries(departments).map(getGroupStatistics);
 
-    return {
-      department: department[0],
-      summary: summaryStatistics.getSummaryStatistics(salaries),
-    };
+  res.send(summary);
+});
+
+router.get('/sub-departments', (req, res) => {
+  const employees = employeeRepository.fetchAll();
+
+  const departments = {};
+
+  employees.forEach((e) => {
+    const index = `${e.department}-${e.sub_department}`;
+
+    if (!departments[index]) {
+      departments[index] = [];
+    }
+
+    departments[index].push(e);
   });
+
+  const summary = Object.entries(departments).map(getGroupStatistics);
 
   res.send(summary);
 });
